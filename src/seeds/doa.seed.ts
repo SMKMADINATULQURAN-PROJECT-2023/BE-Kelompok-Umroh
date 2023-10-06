9efe420b6ffb41c2bd002118e8618065c2bcd19f
Zul Fahmi <Zul@WOWBIDs-MacBook-Pro.local>
2023-10-06 20:56:18 +0700

    fix by hilmi, fix seeder

--- src/seeds/doa.seed.ts
+++ src/seeds/doa.seed.ts
@@ -6,11 +6,13 @@ import { dataDoa } from 'src/utils/json/harian';
 import { Repository } from 'typeorm';
 import { ResponseSuccess } from 'src/interface';
 import BaseResponse from 'src/utils/response/base.response';
+import { ConvertSlugService } from 'src/utils/service/convert_slug/convert_slug.service';
 
 @Injectable()
 export class DoaSeeder extends BaseResponse {
   constructor(
     @InjectRepository(Doa) private readonly doaRepo: Repository<Doa>,
+    private slug: ConvertSlugService,
   ) {
     super();
   }
@@ -19,7 +21,7 @@ export class DoaSeeder extends BaseResponse {
     try {
       const doaData = dataDoa.data;
       for (const data of doaData) {
-        const { name, arab, arti, kategori_id } = data;
+        const { name, arab, arti, kategori_id, slug } = data;
 
         const check = await this.doaRepo.findOne({
           where: {
@@ -34,6 +36,7 @@ export class DoaSeeder extends BaseResponse {
             ...data,
             kategori_id: { id: kategori_id },
           });
+          data.slug = this.slug.slugify(data.name);
           await this.doaRepo.save(doa);
         }
       }
--- src/utils/json/harian.ts
+++ src/utils/json/harian.ts
@@ -3,12 +3,16 @@ export const dataDoa = {
     {
       name: 'Doa Sebelum Makan',
       arab: 'اللَّهُمَّ بارِكْ لَنا فِيما رَزَقْتَنا وَقِنا عَذَابَ النَّارِ',
+      latin: '',
+      slug: '',
       arti: 'Ya Allah berkatilah rezeki yang telah kau limpahkan pada kami, dan peliharalah kami dari azab api neraka.',
       kategori_id: 1,
     },
     {
       name: 'Doa Sesudah Makan',
       arab: 'اَلْحَمْدُ ِللهِ الَّذِيْنَ اَطْعَمَنَا وَسَقَانَا وَجَعَلَنَا مِنَ الْمُسْلِمِيْنَ',
+      latin: '',
+      slug: '',
       arti: 'Segala puji bagi Allah yang telah memberi kami makan dan minum serta menjadikan kami termasuk golongan orang muslim.',
       kategori_id: 1,
     },
