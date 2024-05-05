344fd252af4b8899cfa0a8b91a525e4fb166fc72
larsalfarabi <msyamil404@gmail.com>
2024-05-05 10:18:41 +0700

    env

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,11 +3,11 @@ dotenv.config();
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST_TEST,
   port: parseInt(process.env.PORT), //port default 3306 lihat xampp
-  username: process.env.DB_USERNAME, // username default xampp root
-  password: process.env.DB_PASSWORD, // password default xampp string kosong
-  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME_TEST, // username default xampp root
+  password: process.env.DB_PASSWORD_TEST, // password default xampp string kosong
+  database: process.env.DB_NAME_TEST,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: true,
